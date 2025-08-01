b8d1373541337ae220ced81bd1fb117ff9b254ee
Mihir Samant <47722466+miSamant@users.noreply.github.com>
2025-08-01 17:19:03 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AirflowConfigCalculator from './Airflow-Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AirflowConfigCalculator />
   </React.StrictMode>
 );
