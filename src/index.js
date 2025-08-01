d3cd8fe65736f4f733df16908ced667a332553bc
Mihir Samant <47722466+miSamant@users.noreply.github.com>
2025-08-01 18:40:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AirflowConfigCalculator from './Airflow-Calculator.js';
+import AirflowCalculator from './Airflow-Calculator.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
